20f8d29f9474a9f229c1c48d8e99bb2b946b901f
Alfian Pradana Isanto <pradanaalfian@gmail.com>
2021-03-13 10:54:00 +0700

    Clean

--- resources/views/admin/layout/partial/head.blade.php
+++
@@ -1,6 +0,0 @@
-<link rel="apple-touch-icon" sizes="76x76" href="{{asset('admin/assets/img/apple-icon.png')}}">
-<link rel="icon" type="image/png" href="{{asset('admin/assets/img/favicon.png')}}">
-<!-- CSS Files -->
-<link href="{{asset('admin/assets/css/material-dashboard.css?v=2.1.2')}}" rel="stylesheet" />
-<!-- CSS Just for demo purpose, don't include it in your project -->
-<link href="{{asset('admin/assets/demo/demo.css')}}" rel="stylesheet" />
