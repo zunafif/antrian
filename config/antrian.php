b3adb66b7fa8029043e6a91b0eb21a25758e5af4
Alfian Pradana Isanto <pradanaalfian@gmail.com>
2021-03-22 13:58:59 +0700

    Add config and z_order

--- config/antrian.php
+++ config/antrian.php
@@ -4,5 +4,6 @@ return [
   "nama_rs" => 'Rumah Sakit Bunda Sidoarjo',
   "alamat_rs" => 'Jl. Kundi 70, Kepuh Kiriman, Waru',
   "queue_column" => '4',
-  "default_password" => 'bunda123456'
+  "default_password" => 'bunda123456',
+  "general_counter_name" => '(POLI)'
 ];
