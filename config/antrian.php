29cdf2090ba86b0c6838aedff606fbf7fa3e9f5e
root <root@192.168.1.7>
2021-03-22 14:06:52 +0700

    Add config

---
+++ config/antrian.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	"nama_rs" => 'Rumah Sakit Bunda Sidoarjo',
+	"alamat_rs" => 'Jl. Kundi 70, Kepuh Kiriman, Waru',
+	"queue_column" => '4',
+	"default_password" => 'bunda123456',
+	"general_counter_name" => '(POLI)'
+];
